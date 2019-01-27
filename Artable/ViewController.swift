ce506ea81f8f85a0ba1905b539fdbf4a2e833a83
Rupin Gupta <rupingupta@Rupins-MacBook-Pro.local>
2019-01-27 12:03:24 +0530

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Rupin Gupta on 27/01/19.
+//  Copyright © 2019 Rupin Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
